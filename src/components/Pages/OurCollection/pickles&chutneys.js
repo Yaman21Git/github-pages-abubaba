eba5620f9571e8e255da49feaca739d9ceac23a1
Anidya <54199220+Anidya@users.noreply.github.com>
2021-05-27 14:57:14 +0530

    Update pickles&chutneys.js

--- src/components/Pages/OurCollection/pickles&chutneys.js
+++ src/components/Pages/OurCollection/pickles&chutneys.js
@@ -3,42 +3,27 @@ import {Link} from 'react-router-dom'
 import TrendingItem from '../../TrendingItems'
 import '../../Trending.css'
 
-import img0 from '../../../images2/190Mango-Chutney.jpg'
-import img1 from '../../../images2/191-Mango-Pickle.jpg'
-import img2 from '../../../images2/192-Lemon-Pickle-amended-02.jpg'
-import img3 from '../../../images2/193-Lemon-Black-Pickle.jpg'
-import img4 from '../../../images2/194-Green-Chilli-Pickle.jpg'
-import img5 from '../../../images2/195 Garlic-Pickle.jpg'
-import img6 from '../../../images2/Lemon-Chutney.jpg'
+import img0 from '../../../images2/144-Chitra-Kidney-Beans.jpg'
+import img1 from '../../../images2/154-White-Rice-Basmati.jpg'
+import img2 from '../../../images2/150-Wheat-Flour.jpg'
 
 const index0=[0,1,2];
-const index1=[3,4,5];
-const index2=[6];
-const imgSrc=[img0,img1,img2,img3,img4,img5,img6];
-const text=['Mango Chutney', 'Mango Pickle', 'Lemon Pickle','Lemon Black Pickle','Green Chilli Pickle', 'Garlic Pickle','Lemon Chutney'];
-const price=['₹ 00.00','₹ 00.00','₹ 00.00','₹ 00.00','₹ 00.00','₹ 00.00','₹ 00.00'];
-const path=['/products/60aa6b933f3ca14f407dc623','/products/60aa6cbe3f3ca14f407dc624','/products/60aa6e353f3ca14f407dc625','/products/60aa90202e79914b043dc0ad','/products/60aa6f803f3ca14f407dc626','/products/60aa703d3f3ca14f407dc627','/products/60aa71303f3ca14f407dc628'];
+
+const imgSrc=[img0,img1,img2]
+const text=['Pulses', 'Rice', 'Flour'];
+const path=['/category/pulses','/category/rice','/category/flour'];
+const show=[true, true, true];
 
 export default function Pickles(){
      return(
          <>
            <div className='trends'>
-           <h1 style={{marginBottom:"5%"}}>Pickles & Chutney Tales</h1>
+           <h1 style={{marginBottom:"5%"}}>Ancient Living</h1>
             <div className='trends_container'>
                 <div className='trends_wrapper'>
                 <ul className='trends_items'>
                 {index0.map(i => {
-                    return <TrendingItem src={imgSrc[i]} text={text[i]} path={path[i]} price={price[i]}/>
-                    })}
-                </ul>
-                <ul className='trends_items'>
-                {index1.map(i => {
-                    return <TrendingItem src={imgSrc[i]} text={text[i]} path={path[i]} price={price[i]}/>
-                    })}
-                </ul>
-                <ul className='trends_items' style={{width:"33%"}}>
-                {index2.map(i => {
-                    return <TrendingItem src={imgSrc[i]} text={text[i]} path={path[i]} price={price[i]}/>
+                    return <TrendingItem src={imgSrc[i]} text={text[i]} path={path[i]} show={show[i]}/>
                     })}
                 </ul>
                 </div>
