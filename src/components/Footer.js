df743a7d2baa6c8c9f64c613751245b5c4ee3659
Anidya <54199220+Anidya@users.noreply.github.com>
2021-05-27 17:01:38 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,138 +1,138 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Footer.css';
 import '../App.css';
 import { Button } from './button';
 import { Link } from 'react-router-dom';
+import { sendQuery } from './Pages/product';
 
-function Footer() {
-  return (
-    <>
-      <div className="footer-container">
-        <div className="footerWrapper">
-        <hr></hr>
-         <div className="socialMedia">
-           <h2>Connect with us</h2>
-           <div className="social-media">
-         <Link class='social-icon-link facebook'
-                to='/'
-                target='_blank'
-                aria-label='Facebook'
-          >
-          <i class='fab fa-facebook-f' />
-          </Link>
-                    <Link
-                    class='social-icon-link instagram'
-                    to='/'
-                    target='_blank'
-                    aria-label='Instagram'
-                    >
-                    <i class='fab fa-instagram' />
-                    </Link>
-                    <Link
-                    class='social-icon-link youtube'
-                    to='/'
-                    target='_blank'
-                    aria-label='Youtube'
-                    >
-                    <i class='fab fa-youtube' />
-                    </Link>
-                    <Link
-                    class='social-icon-link twitter'
-                    to='/'
-                    target='_blank'
-                    aria-label='Twitter'
-                    >
-                    <i class='fab fa-twitter' />
-                    </Link>
-                    <Link
-                    class='social-icon-link twitter'
-                    to='/'
-                    target='_blank'
-                    aria-label='LinkedIn'
-                    >
-                    <i class='fab fa-linkedin' />
-                    </Link></div>
-            </div>
-            <div className="footerInfo">
-              <h2>Information</h2>
-              <p><a href="/">Home</a></p>
-              <p><a href="/contactus">About Us</a></p>
-              <p><a href="/services">Shop</a></p>
-              <p><a href="/Blogs">Blogs</a></p>
-              <p><a href="/">FAQs</a></p>
-              <p><a href="/">Shipping Information</a></p>
-              <p><a href="/">Return Policy</a></p>
-              <p><a href="/">Terms and Conditions</a></p>
-              <p><a href="/">Privacy Policy</a></p>
-            </div>
-            <div className="contactInfo">
-                <h2>Contact us</h2>
-                <form className="reachus">
-                   <p><input className="footer-input" type="name" placeholder="Full Name*" name="name"></input></p>
-                   <p><input className="footer-input" type="email" placeholder="Email Address*" name="email"></input></p>
-                   <p><input className="footer-input" type="text" placeholder="Message*" name="message"></input></p>
-                   <Link to="/"><button className="btnsubmit">Submit</button></Link>
-                </form>
-            </div>
+class Footer extends Component {
+  constructor(){
+      super()
+      this.state = {
+          name: "",
+          email: "",
+          message: ""
+      }
+  }
 
-            <div className="footerInfo2 hidden-info">
-              <h2>Information</h2>
-              <p><a href="/">Home</a></p>
-              <p><a href="/contactus">About Us</a></p>
-              <p><a href="/services">Shop</a></p>
-              <p><a href="/Blogs">Blogs</a></p>
-              <p><a href="/">FAQs</a></p>
-              <p><a href="/">Shipping Information</a></p>
-              <p><a href="/">Return Policy</a></p>
-              <p><a href="/">Terms and Conditions</a></p>
-              <p><a href="/">Privacy Policy</a></p>
-            </div>
-         </div>
-      </div>
-    </>
-    // <div className='footer-container'>
-    //   <div class='footer-links'>
-    //     <div className='footer-link-wrapper'>
-    //       <div class='footer-link-items'>
-    //         <section class='social-media'>
-    //           <div class='social-media-wrap'>
-    //             <div class='social-icons'>
-    //                 
-    //             </div>
-    //             </div>
-    //         </section>
-    //       </div>
-    //     </div>
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleClick = () => {
+      const {name, email, message} = this.state;
+
+      const query = {
+          name: name,
+          email: email,
+          message: message
+      }
+      
+      sendQuery(query)
+        .then(result => {
+            alert(result);
+            this.setState({
+                name: "",
+                email: "",
+                message: ""
+            })
+        })
+  }
 
 
-    //     <div className='footer-link-wrapper'>
-    //       <div class='footer-link-items'>
-    //         <h2><a href="/contactus">About Us</a></h2>
-    //         <Link to='/sign-up'>How it works</Link>
-    //         <Link to='/'>Testimonials</Link>
-    //         <Link to='/'>Careers</Link>
-    //         <Link to='/'>Investors</Link>
-    //         <Link to='/'>Terms of Service</Link>
-    //       </div>
-    //       <div class='footer-link-items'>
-    //         <section className='footer-subscription'>
-    //             <p className='footer-subscription-text'>
-    //             Contact Us
-    //             </p>
-    //             <div className='input-areas'>
-    //             <form>
-    //                 <input className='footer-input' name='email' type='email' placeholder='Your Email'/>
-    //                 <input className='footer-input' name='name' type='text' placeholder='Your Name'/>
-    //                 <input className='footer-input' name='Message' type='text' placeholder='Message'/>
-    //                 <Button buttonStyle='btn--outline'>Submit</Button>
-    //             </form>
-    //             </div>
-    //         </section>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-  );
+  render() {
+    const {message, email, name} = this.state
+    return (
+          <>
+            <div className="footer-container">
+              <div className="footerWrapper">
+              <hr></hr>
+              <div className="socialMedia">
+                <h2>Connect with us</h2>
+                <div className="social-media">
+              <Link class='social-icon-link facebook'
+                      to='/'
+                      target='_blank'
+                      aria-label='Facebook'
+                >
+                <i class='fab fa-facebook-f' />
+                </Link>
+                          <Link
+                          class='social-icon-link instagram'
+                          to='/'
+                          target='_blank'
+                          aria-label='Instagram'
+                          >
+                          <i class='fab fa-instagram' />
+                          </Link>
+                          <Link
+                          class='social-icon-link youtube'
+                          to='/'
+                          target='_blank'
+                          aria-label='Youtube'
+                          >
+                          <i class='fab fa-youtube' />
+                          </Link>
+                          <Link
+                          class='social-icon-link twitter'
+                          to='/'
+                          target='_blank'
+                          aria-label='Twitter'
+                          >
+                          <i class='fab fa-twitter' />
+                          </Link>
+                          <Link
+                          class='social-icon-link twitter'
+                          to='/'
+                          target='_blank'
+                          aria-label='LinkedIn'
+                          >
+                          <i class='fab fa-linkedin' />
+                          </Link></div>
+                  </div>
+                  <div className="footerInfo">
+                    <h2>Information</h2>
+                    <p><a href="/">Home</a></p>
+                    <p><a href="/contactus">About Us</a></p>
+                    <p><a href="/services">Shop</a></p>
+                    <p><a href="/Blogs">Blogs</a></p>
+                    <p><a href="/">FAQs</a></p>
+                    <p><a href="/">Shipping Information</a></p>
+                    <p><a href="/">Return Policy</a></p>
+                    <p><a href="/">Terms and Conditions</a></p>
+                    <p><a href="/">Privacy Policy</a></p>
+                  </div>
+                  <div className="contactInfo">
+                      <h2>Contact us</h2>
+                      <form className="reachus">
+                        <p><input className="footer-input" type="name" placeholder="Full Name*" name="name" value={name} onChange={this.handleChange}></input></p>
+                        <p><input className="footer-input" type="email" placeholder="Email Address*" name="email" value={email} onChange={this.handleChange}></input></p>
+                        <p><input className="footer-input" type="text" placeholder="Message*" name="message" value={message} onChange={this.handleChange}></input></p>
+                        <Link to="/"><button className="btnsubmit" onClick={this.handleClick}>Submit</button></Link>
+                        </form>
+                  </div>
+
+                  <div className="footerInfo2 hidden-info">
+                    <h2>Information</h2>
+                    <p><a href="/">Home</a></p>
+                    <p><a href="/contactus">About Us</a></p>
+                    <p><a href="/services">Shop</a></p>
+                    <p><a href="/Blogs">Blogs</a></p>
+                    <p><a href="/">FAQs</a></p>
+                    <p><a href="/">Shipping Information</a></p>
+                    <p><a href="/">Return Policy</a></p>
+                    <p><a href="/">Terms and Conditions</a></p>
+                    <p><a href="/">Privacy Policy</a></p>
+                  </div>
+              </div>
+            </div>
+          </>
+
+    );
+  }
 }
-
 export default Footer;
