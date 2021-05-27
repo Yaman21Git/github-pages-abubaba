db2f82f7393325f54990926d01508879f036aa92
Anidya <54199220+Anidya@users.noreply.github.com>
2021-05-27 15:13:42 +0530

    Update Search.js

--- src/components/Pages/Search.js
+++ src/components/Pages/Search.js
@@ -1,48 +1,73 @@
 import React, { Component } from 'react'
 import '../../App.css'
-import { Button } from '../button'
 import '../Footer.css'
-import {Link} from 'react-router-dom';
 import {searchByWord} from './product'
+import TrendingItem from '../TrendingItems'
+import '../Trending.css'
 
 class Search extends Component{
     constructor(){
         super()
         this.state = {
             word: "",
-            products: ""
+            products: "",
+            index: ""
         }
     }
 
     handleClick = () => {
-        const {word} = this.state;
+        var {word, index} = this.state;
         const key = {word: word};
         searchByWord(key)
         .then(data => {
-          if(data.error)
-              console.log(data.error);
-          else
-              this.setState({products: data}) 
-      })
+            if(data.error)
+                console.log(data.error);
+            else{
+
+                this.setState({products: data})
+            
+                const sz = data.length;
+                var x = parseInt( (sz + 2)/3 );
+                index = new Array(x);
+                for(var i=0; i<x; i++){
+                    index[i] = new Array(3);
+                    for(var j=0; j<3; j++){
+                        if(i*3 + j == sz)
+                            break;
+                        index[i][j] = i * 3 + j;
+                    }
+                }
+                this.setState({index: index});
+            }
+        })
     }
 
     render(){
-      const {word, products} = this.state
+      const {word, products, index} = this.state
       console.log(products, word);
       return (
         <>
-          <div className="search-item-container">
+            <div className='search-item-container'>
               <div className="navbar-hidden">
-                <div style={{width:"50%",float:"left",paddingTop:"2%"}}><h1 style={{marginLeft:"8%", textAlign:"left",fontSize:"4vw"}}>Your searched items will appear below:</h1></div>
-                <div className="search-btn">
-                  <input className="main-search" placeholder="What do you want to search?" name="word" value={word} onChange = {(event) => this.setState({word: event.target.value})}></input>
-                  <button className="redbtn" onClick={this.handleClick}>SEARCH</button>
+                <div className="search-btn"> 
+                    <input className="main-search" placeholder="What do you want to search?" name="word" value={word} onChange = {(event) => this.setState({word: event.target.value})}></input>
+                    <button className="redbtn" onClick={this.handleClick}>SEARCH</button>
                 </div>
-                { products && products.map((product, i) => (
-                  <p>{product.name}</p>
-                ))}
-              </div> 
-          </div>
+              </div>
+              <div style={{width:"50%", paddingTop:"2%"}}><h1 style={{marginLeft:"8%", textAlign:"left",fontSize:"4vw"}}>Your searched items will appear below:</h1></div>
+              <br/><br/>
+                {index && <div className='trends_container'>
+                    <div className='trends_wrapper'>
+                      {index.map( (array, i) => (
+                          <ul className='trends_items' style={{width: !index[i][1] ? "33%" : (!index[i][2] ? "67%" : "100%") }}>
+                          { array.map( (val, j) => (
+                              <TrendingItem src={products[val].photos[0]} text={products[val].name} path={`/products/${products[val]._id}`} price={products[i].price}/>
+                          ))}
+                          </ul>
+                      ))}   
+                    </div>
+                </div>}
+            </div> 
         </>
       );
     }
